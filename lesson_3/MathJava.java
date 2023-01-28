e13cf382ed0c57f0e910ce65567eaa417ea23c09
Pham Quoc Cuong <cuongpq.fit@gmail.com>
2023-01-28 21:34:18 +0700

    lesson 3

--- lesson_3/MathJava.java
+++
@@ -1,9 +0,0 @@
-public class MathJava {
-
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
-        System.out.println("Ramdom: " + Math.random());
-        System.out.println("Max: " + Math.max(a, b));
-    }
-}
