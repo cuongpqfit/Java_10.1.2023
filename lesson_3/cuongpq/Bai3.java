a4344c5678d37a27a7ee2ccd9f93ec033b0e5333
Pham Quoc Cuong <cuongpq.fit@gmail.com>
2023-01-31 21:56:26 +0700

    Lesson 4

--- lesson_3/cuongpq/Bai3.java
+++ lesson_3/cuongpq/Bai3.java
@@ -1,8 +1,17 @@
-package PhamBaMinh;
+package cuongpq;
 
 import java.util.Scanner;
 
 public class Bai3 {
+
+    public static void showMatrix(int[][] matrix, int m, int n) {
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int row1, col1, row2, col2, row, col;
@@ -33,9 +42,41 @@ public class Bai3 {
                 matrix2[i][j] = scanner.nextInt();
 
             }
-
         }
 
+        System.out.println("Show matrix 1:");
+        showMatrix(matrix1, row1, col1);
 
+        System.out.println("Show matrix 2:");
+        showMatrix(matrix2, row2, col2);
+
+        if ((row1 != row2) || (col1 != col2)) {
+            System.out.println("khong cong dc");
+            return;
+        }
+
+        int[][] sumMatrix = new int[100][100];
+        for (int i=0; i<row1; i++) {
+            for (int j=0; j<col1; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        System.out.println("Sum matrix:");
+        showMatrix(sumMatrix, row1, col1);
     }
 }
+
+//A(m x n) + B(m x n)
+//Am == Bm && An == Bn
+//
+//A       B
+//2 3     1 1
+//1 1     1 1
+//
+//A + B =
+//(2+1)  (3 + 1)
+//(1 + 1)   (1 + 1)
+//
+//3 4
+//2 2
