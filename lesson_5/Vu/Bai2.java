40d41d60e7154b5003758f96c352ba1c8e69e073
vule <vule@vulenoMacBook-Air.local>
2023-02-04 20:54:47 +0900

    commit truoc b2,b3

---
+++ lesson_5/Vu/Bai2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Bai2 {
+    
+public static void main(String[]agrs) {
+
+    //sap xep theo chieu giam dan
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println(" hay nhap so n =");
+    int n = scan.nextInt();
+ 
+    int [] a = new int[n];
+
+    for (int i = 0; i<n ; i++){
+     a[i] = scan.nextInt();
+ 
+    }
+
+    // for (int i : a)
+    //     System.out.print(i+" ");
+    //     System.out.println();
+
+    //sap xep theo chieu giam dan
+    int temp;
+    int last, j;
+    for (int i=1; i<n; i++) {
+      last = a[i];
+        j = i;
+        while ((j>0) &&(a[j-1])< last){
+            temp = a[j];
+            a[j]= a[j-1];
+            a[j-1]=temp;
+
+            j = j-1;
+        }
+        a[j] = last;
+            
+        }
+
+    // output
+
+    for (int i : a)
+    System.out.print(i+" ");
+}
+    
+}
+
