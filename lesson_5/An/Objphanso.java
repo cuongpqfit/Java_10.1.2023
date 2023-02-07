0da79d02ed2024934e3fa2a0c971889b7e64f77e
TranCongAn94 <jiit17082@gmail.com>
2023-02-07 11:03:01 +0900

    lesson5

---
+++ lesson_5/An/Objphanso.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ojbphanso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tử số : ");
+        int tuso = sc.nextInt();
+        System.out.println("Nhập mẫu số : ");
+        int mauso = sc.nextInt();
+        Phanso objPhanso1 = new Phanso();
+        Phanso objPhanso2 = new Phanso();
+        Phanso objPhanso3 = new Phanso();
+    }
+}
