1297b028f2e11f00e71fc12e0b9641b9bfa8dfd5
DAMPHUCbka <damphucbka@gmail.com>
2023-02-28 19:50:52 +0900

    commit lesson 12

--- lesson_12/PhucDV/Server.java
+++ lesson_12/PhucDV/Server.java
@@ -3,15 +3,24 @@ package lesson_12.PhucDV;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Server {
+    private static List<SocketServerService> listClient =  new ArrayList<>();
     public static void main(String[] args) throws IOException {
+
         try {
             ServerSocket serverSocket = new ServerSocket(1996);
             while (true){
-                SocketServerService socketServerService = new SocketServerService(serverSocket);
-                Thread thr1 = new Thread(socketServerService);
-                thr1.start();
+                System.out.println("Client connected: " + serverSocket.getInetAddress().getHostAddress());
+                Socket clientSocket = serverSocket.accept();
+                SocketServerService client = new SocketServerService(clientSocket);
+                client.start();
+                System.out.println("XXXXXXXXX");
+                listClient.add(client);
+
             }
         }catch (IOException ex){
             System.out.println(ex.getMessage());
